961de183be381fdac379be11abeebe78e7ab1bc4
danofoundation <dan.dev947366@gmail.com>
2024-08-04 19:56:15 +0300

    Update projects

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,10 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+projectList = [
+    {
+        "id": "1",
+        "title": "Ecommerce website",
+        "description": "fully functional ecommerce web",
+    },
+    {
+        "id": "2",
+        "title": "Porfolio website",
+        "description": "A porfolio website",
+    },
+    {
+        "id": "3",
+        "title": "Content management",
+        "description": "Content management website",
+    },
+]
+
 
 def projects(request):
-    return render(request, "projects.html")
+    page = "projects"
+    number = 9
+    context = {"page": page, "number": number, "projects": projectList}
+    return render(request, "projects/projects.html", context)
 
 
 def project(request, pk):
-    return render(request, "single-project.html")
+    projectObj = None
+    for i in projectList:
+        if i["id"] == pk:
+            projectObj = i
+    return render(request, "projects/single-project.html", {"project": projectObj})
